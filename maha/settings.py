2f3348b495463cb71ec04aa78f359bf483e324e7
Lekan8562 <138625724+Lekan8562@users.noreply.github.com>
2024-05-09 01:49:46 +0100

    settings update

--- maha/settings.py
+++ maha/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-kr$+!+-nuvxtw9#3@k(txi_$$ilo(#=b5j3_3s2u2l89o5j1an
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -121,8 +121,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_URL = '/staticfiles/'
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 
